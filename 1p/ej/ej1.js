7fbb7a73c5b1a3b3691993d0d2f77da39fcee9d5
yovanha <yfajardo0@ucol.mx>
2020-10-18 16:41:02 -0500

    ejercicio 2

--- 1p/ej/ej1.js
+++ 1p/ej/ej1.js
@@ -1,9 +1,7 @@
+//Serie de números impares 1, 3, 5, ...
 var x = 1;
 var n = 10;
 for (let i = 1; i <= n; i++){
-
-    x += 2;
     console.log(x);
+    x += 2;
 }
-
-
---
+++ 1p/ej/ej2.js
@@ -0,0 +1,11 @@
+//Serie de números impares con diferente signo -1, 3, -5, ...
+var x = -1;
+var n = 10;
+var y = 2;
+for (let i = 1; i <= n; i++){
+    console.log(x);
+    y *= -1;
+    x += y;
+    x *= -1;
+}
+
