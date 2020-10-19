ba192845c1297c2fe29fb04af4777414ba231fe9
yovanha <yfajardo0@ucol.mx>
2020-10-19 13:26:35 -0500

    terminado

--- 1p/ej/ej9.js
+++ 1p/ej/ej9.js
@@ -1,9 +1,9 @@
 //Factorial
 var n = 10;
 var num1;
-var factoreal = 1;
+var factorial = 1;
 for (let i = 1; i<=n; i++){
-    console.log (factoreal);
+    console.log (factorial);
     num1 = i + 1;
-    factoreal = factoreal * num1;
+    factorial = factorial * num1;
 }
