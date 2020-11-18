5e479c7d61114d59c96e5eec4b797655c762d6d0
yovanha <yfajardo0@ucol.mx>
2020-11-18 11:58:28 -0600

    terminado

--- 1p/consola/ln2.js
+++ 1p/consola/ln2.js
@@ -3,3 +3,15 @@ var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+var ln = 0 
+var l = 0
+rl.question('Introduce x: ', (x) => {
+rl.question('Introduce el número de iteraciones: ', (n) => {
+for( i = 1; i <= n; i++){
+  l = (Math.pow(x, i)*Math.pow(-1,i+1))/i;
+  ln = l;
+  rl.close();
+}
+console.log(ln);
+})
+})
