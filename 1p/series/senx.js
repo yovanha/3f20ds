56971d17c0290730064860a06c72e2a00286c832
yovanha <yfajardo0@ucol.mx>
2020-11-18 11:20:27 -0600

    terminado

--- 1p/series/senx.js
+++ 1p/series/senx.js
@@ -1,12 +1,17 @@
 //sen x = x - x^3/3! + ... -
-var y;
-var x;
+var senx;
+var sen = 0;
+var x = Math.PI/2;
 var n = 10;
-for (let i = 1; i <= n; i++){
-    y = y+((-1)^i)*(x^(2*i+1))/factorial(2*i+1)
+var f = 1;
+function factorial(){
+    for (let k=1; k<n; k++){
+        f = f * k;
+        return f;
+    }
 }
-console.log(y);
-//sin(x):
-//for i=0:10,
-//y = y+((-1)^i)*(x^(2*i+1))/factorial(2*i+1);
-//end
+for (let i = 1; i <= n; i++){
+    senx = Math.pow(-1,i)*(Math.pow(x,2*i+1)/factorial(2*i+1));
+    sen += senx;
+    console.log(sen);
+}
