0e7e4d89b95e37b3096e8dacbddbad582b0b1865
yovanha <yfajardo0@ucol.mx>
2020-11-18 11:24:13 -0600

    terminado

---
+++ 1p/arrays/senx.js
@@ -0,0 +1,19 @@
+//sen x = x - x^3/3! + ... -
+var senx;
+var sen = 0;
+var x = Math.PI/2;
+var n = 10;
+var f = 1;
+var vector = [];
+function factorial(){
+    for (let k=1; k<n; k++){
+        f = f * k;
+        return f;
+    }
+}
+for (let i = 1; i <= n; i++){
+    senx = Math.pow(-1,i)*(Math.pow(x,2*i+1)/factorial(2*i+1));
+    sen += senx;
+    vector[i] = sen;
+}
+console.log(vector);
