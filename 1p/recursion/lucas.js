ecd52e280604bc548a48938b188979dd001d2b86
yovanha <yfajardo0@ucol.mx>
2020-11-19 01:00:40 -0600

    terminado

---
+++ 1p/recursion/lucas.js
@@ -0,0 +1,7 @@
+//lucas sucesión 2,1,3,4,7...
+function lucas(num) {
+    if (num == 1) return 2;
+    if (num == 2) return 1;
+    return lucas(num - 1) + lucas(num - 2);
+  }
+  console.log(lucas(5))
